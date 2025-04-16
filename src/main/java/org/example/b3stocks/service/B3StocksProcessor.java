e351c3e04c8fbddb2f8c4e98badb90e03fc11406
Gustavo Lelis <gustavolelis15@gmail.com>
2025-04-15 21:20:02 -0300

    arquivos csv separados

--- src/main/java/org/example/b3stocks/service/B3StocksProcessor.java
+++ src/main/java/org/example/b3stocks/service/B3StocksProcessor.java
@@ -13,16 +13,17 @@ public class B3StocksProcessor {
     private static final String INPUT_FILE = "b3_stocks_1994_2020.csv";
     private static final String INPUT_FILE_T1 = "src\\main\\resources\\b3stocks_T1.csv";
     private static final String INPUT_FILE_F1 = "src\\main\\resources\\b3stocks_F1.csv";
+    private static final String INPUT_FILE_AT1 = "src\\main\\resources\\b3stocks_AT1.csv";
 
     public void executarTodasTransformacoes() throws IOException {
-        DataBovespa[] dados = CSVUtils.readFromCSV(INPUT_FILE);
 
+        DataBovespa[] dados = CSVUtils.readFromCSV(INPUT_FILE);
         CSVUtils.writeToCSV(INPUT_FILE_T1, dados);
 
         DataBovespa[] dadosFiltradosF1 = F1VolumeMaxFilter.getMaxVolumeRecordPerDay(dados);
         CSVUtils.writeToCSV(INPUT_FILE_F1, dadosFiltradosF1);
 
         DataBovespa[] dadosFiltradosF2 = F2VolumeAboveAverage.getHigherValueDailyAverage(dados);
-        CSVUtils.writeToCSV(INPUT_FILE_T1, dadosFiltradosF2);
+        CSVUtils.writeToCSV(INPUT_FILE_AT1, dadosFiltradosF2);
     }
 }
